39d480dc2179bd2a0033347cbb2e0f6aa4c44484
Andres Marcelo <eandres@linux>
2020-08-22 12:07:28 -0500

    Checking the problem: 3n+1

--- Problem_3n+1.c
+++ Problem_3n+1.c
@@ -1,15 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(){
-    char a[1024];
-    char b[1024];
+int main(int argc, char** argv){
 
-    printf("+ Ingresa el primer valor:");
-    scanf("%s[^\n]",a);
-    printf("+ Ingresa el segundo valor:");
-    scanf("%s[^\n]",b);
-
-    printf("%s + %s = C\n",a,b);
+    int a;
+    int b;
+    int i;
+    int n;
+    int count = 0;
+    char buffer[20];
+    FILE *fout = fopen("data.out","w+");
+    while(fgets(buffer,sizeof(buffer),stdin)) {
+        sscanf(buffer,"%d %d[^\n]",&a,&b);        
+        int max = 1;
+        if(a>0 && b>0 && a<1000000 && b<1000000 && b>a){
+            for(i=n=a; i<=b; count++){
+                if(n==1){
+                    if(count > max)
+                        max = count;
+                    count = 0;
+                    i++;
+                    n=i;
+                    continue;
+                }
+                if(n%2)
+                    n = (3*n)+1;
+                else
+                    n /= 2;      
+            }
+            fprintf(fout,"%d %d %d\n",a,b,max);
+        }
+    };
+    fclose(fout);
     return 0;
 }
