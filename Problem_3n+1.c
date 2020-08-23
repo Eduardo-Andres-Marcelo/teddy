361519bcba7570e8cb4cf5a82ef52796b619ed29
Eduardo-Andres-Marcelo <eandresmarcelo@gmail.com>
2020-08-22 21:08:55 -0500

    Wrong answer...

--- Problem_3n+1.c
+++ Problem_3n+1.c
@@ -7,15 +7,24 @@ int main(int argc, char** argv){
     int b;
     int i;
     int n;
+    int max;
     int count = 0;
     char buffer[20];
     FILE *fout = fopen("data.out","w+");
     while(fgets(buffer,sizeof(buffer),stdin)) {
-        sscanf(buffer,"%d %d[^\n]",&a,&b);        
-        int max = 1;
-        if(a>0 && b>0 && a<1000000 && b<1000000 && b>a){
-            for(i=n=a; i<=b; count++){
+        sscanf(buffer,"%d %d",&a,&b);        
+        max = 1;
+        if(a>0 && b>0 && a<1000000 && b<1000000){
+            if(a>b){
+                i = b;
+                b = a;
+                a = i;
+            }
+            i=n=a;
+            while(i<=b){
+                count ++;
                 if(n==1){
+                    
                     if(count > max)
                         max = count;
                     count = 0;
@@ -23,11 +32,15 @@ int main(int argc, char** argv){
                     n=i;
                     continue;
                 }
-                if(n%2)
+
+                
+                if(n&1)
                     n = (3*n)+1;
                 else
-                    n /= 2;      
-            }
+                    n >>= 1;      
+                
+                
+            };
             fprintf(fout,"%d %d %d\n",a,b,max);
         }
     };
