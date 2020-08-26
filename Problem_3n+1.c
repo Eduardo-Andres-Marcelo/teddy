b7ab73284c91a73d298faf1bd0d19113083a7ad8
Eduardo-Andres-Marcelo <eandresmarcelo@gmail.com>
2020-08-25 22:44:21 -0500

    Print result (data.out)

--- Problem_3n+1.c
+++ Problem_3n+1.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 int main(int argc, char** argv){
 
@@ -10,10 +11,18 @@ int main(int argc, char** argv){
     int max;
     int count = 0;
     char buffer[20];
+    char buff_out[9024];
+    char sentence[24];
     FILE *fout = fopen("data.out","w+");
-    while(fgets(buffer,sizeof(buffer),stdin)) {
-        sscanf(buffer,"%d %d",&a,&b);        
+    FILE *fin = fopen("data.in","r");
+    buff_out[0] = '\0';
+    while(fgets(buffer,sizeof(buffer),fin)) {
+        
+	if(buffer[0] == '\n' || buffer[0] == '\t' || buffer[0] == ' ')
+	    continue;
+	sscanf(buffer,"%d %d",&a,&b);        
         max = 1;
+	sentence[0] = '\0';
         if(a>0 && b>0 && a<1000000 && b<1000000){
             if(a>b){
                 i = b;
@@ -23,14 +32,13 @@ int main(int argc, char** argv){
             i=n=a;
             while(i<=b){
                 count ++;
-                if(n==1){
-                    
+                if(n==1){ 
                     if(count > max)
                         max = count;
-                    count = 0;
-                    i++;
-                    n=i;
-                    continue;
+                   count = 0;
+                   i++;
+                   n=i;
+                   continue;
                 }
 
                 
@@ -41,9 +49,11 @@ int main(int argc, char** argv){
                 
                 
             };
-            fprintf(fout,"%d %d %d\n",a,b,max);
+	    sprintf(sentence,"%d %d %d\n",a,b,max);
+	    strcat(buff_out,sentence);
         }
     };
+    fprintf(fout,buff_out);
     fclose(fout);
     return 0;
 }
